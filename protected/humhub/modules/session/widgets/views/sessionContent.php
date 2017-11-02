46443aac9e0792c4b9227fdaff8c25b78a136f43
abhinayjs <abhinayjscool4u@gmail.com>
2017-11-01 21:36:38 -0500

    Adding and Subtracting tokens

--- protected/humhub/modules/session/widgets/views/sessionContent.php
+++ protected/humhub/modules/session/widgets/views/sessionContent.php
@@ -20,23 +20,50 @@ use humhub\modules\admin\widgets\SpaceGridView;
         // 'filterModel' => $searchModel,
         'columns' => [
             'user.username',
-            'tokens',
+            [
+            		'attribute' => 'tokens',
+                    'options' => ['style' => 'width:100px;'],
+                    'format' => 'raw',
+                    'value' => function($data) {
+                        return Html::textInput('tokens', $data->tokens, ['id' => 'tokenId','class' => 'form-control', 'readOnly' => true]);
+                    },
+            ],
             [
                     'header' => Yii::t('AdminModule.views_user_index', 'Actions'),
                     'class' => 'yii\grid\ActionColumn',
                     'options' => ['style' => 'width:80px; min-width:80px;'],
                     'buttons' => [
                         'view' => function($url, $model) {
-                            return Html::a('<i class="fa fa-plus"></i>', ['index'],['class' => 'btn btn-success btn-xs tt']);
+                            return Html::button('<i class="fa fa-plus"></i>',['id' => 'add', 'class' => 'btn btn-success btn-sm tt']);
                         },
-                        'update' => function() {
+                        'update' => function($model) {
                             return;
                         },
                         'delete' => function($url, $model) {
-                            return Html::a('<i class="fa fa-minus"></i>', ['index'], ['class' => 'btn btn-danger btn-xs tt']);
+                            return Html::button('<i class="fa fa-minus"></i>', ['id' => 'subtract', 'class' => 'btn btn-danger btn-sm tt']);
                         }
                     ],
             ],
         ],
     ]);
 ?>
+
+<script type="text/javascript">
+	$('#add').click(function(){
+		var old_value = parseInt(document.getElementById('tokenId').value);
+		document.getElementById('tokenId').value = parseInt(old_value) + 1;
+	});
+
+	$('#subtract').click(function(){
+		var old_value = parseInt(document.getElementById('tokenId').value);
+		if (old_value <= 0)
+		{
+			document.getElementById('tokenId').value = 0;	
+		}
+		else 
+		{
+			document.getElementById('tokenId').value = old_value - 1;
+		}
+	});
+
+</script>
