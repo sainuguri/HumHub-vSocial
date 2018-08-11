156e6a32bc155ead50d4d81673dc4b22fcadf91b
snd45 <snd45@node-0.humhub-snd45.ch-geni-net.instageni.rnet.missouri.edu>
2018-08-11 11:29:20 -0500

    First commit

--- protected/humhub/modules/session/models/SessionSearch.php
+++ protected/humhub/modules/session/models/SessionSearch.php
@@ -48,16 +48,16 @@ class SessionSearch extends Session
         $user = User::findOne(['id' => Yii::$app->user->id]);
         $role = $user->getRoleName();
 
-        if ($role == 'Teacher')
-        {
+//        if ($role == 'Teacher')
+  //      {
             $query = Session::find()->where(['instructor_name'=>Yii::$app->user->getIdentity()->displayname]);
-        }
+    //    }
 
-        if ($role == 'Student') 
-        {
-            $memberships = SessionMembership::find()->select(['session_id'])->where(['user_id' => Yii::$app->user->id]);
-            $query = Session::find()->where(['id'=> $memberships]);
-        }
+//        if ($role == 'Student') 
+  //      {
+    //        $memberships = SessionMembership::find()->select(['session_id'])->where(['user_id' => Yii::$app->user->id]);
+//          $query = Session::find()->where(['id'=> $memberships]);
+      //  }
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
